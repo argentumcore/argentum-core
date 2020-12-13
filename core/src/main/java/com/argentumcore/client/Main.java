b84423c8958b51e14a81a0ee378b63458deb9bf7
Franco Llamas <francollamas077@gmail.com>
2020-12-13 18:59:42 -0300

    Se crea un proyecto de libGDX
    El proyecto fue creado con el generador de proyectos 'gdx-liftoff'
    que tiene mas caracteristicas a diferencia del generador oficial.

---
+++ core/src/main/java/com/argentumcore/client/Main.java
@@ -0,0 +1,7 @@
+package com.argentumcore.client;
+
+import com.badlogic.gdx.ApplicationAdapter;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class Main extends ApplicationAdapter {
+}
