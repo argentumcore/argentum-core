7a89354237e9d861165d993baf5ce4016e209d10
Franco Llamas <francollamas077@gmail.com>
2020-12-13 20:15:29 -0300

    Cambios de nomenclatura
    Se cambio el modulo lwjgl3 por desktop (ya que es mas descriptivo)
    Se cambio el nombre del proyecto en la app de Android a 'Argentum Core'

--- desktop/src/main/java/com/argentumcore/client/desktop/DesktopLauncher.java
+++ desktop/src/main/java/com/argentumcore/client/desktop/DesktopLauncher.java
@@ -1,11 +1,11 @@
-package com.argentumcore.client.lwjgl3;
+package com.argentumcore.client.desktop;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.argentumcore.client.Main;
 
 /** Launches the desktop (LWJGL3) application. */
-public class Lwjgl3Launcher {
+public class DesktopLauncher {
 	public static void main(String[] args) {
 		createApplication();
 	}
