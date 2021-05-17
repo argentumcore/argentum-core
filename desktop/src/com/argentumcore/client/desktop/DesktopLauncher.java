2a96ad0de51e35c822d2cf86c399fc223dbaa040
Franco Llamas <francollamas077@gmail.com>
2021-05-17 19:38:28 -0300

    Configuro el despliegue en todas las plataformas
    - En Desktop hay una nueva task (jpackageImage) para crear paquetes ya
     listos segun el sistema operativo.
    - En Mac OS por alguna razon no funciona el paquete con lwjgl3 (estimo que
     tiene algo que ver con XstartOnFirstThread)
    - Iconos de desktop y android seteados (creado por Gorlok).

--- desktop/src/com/argentumcore/client/desktop/DesktopLauncher.java
+++ desktop/src/com/argentumcore/client/desktop/DesktopLauncher.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setWindowIcon("icons/icon128.png", "icons/icon64.png", "icons/icon32.png", "icons/icon16.png");
 		new Lwjgl3Application(new Game(), config);
 	}
 }
